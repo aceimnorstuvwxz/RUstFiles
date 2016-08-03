a759f571a3e185de2fc9a470305ba41a8c1a8f96
Chen Bingfeng <iichenbf@gmail.com>
2016-08-03 23:40:18 +0800

    g

--- setup.sh
+++ setup.sh
@@ -11,6 +11,24 @@ git config --global user.email dgkae.cbf@gmail.com
 # VS CODE
 
 
+#samba
+sudo apt-get install samba
+
+#sudo vim /etc/samba/smb.conf
+#sudo service smbd restart
+#sudo smbpasswd -a smbuser #与 Linux 用户同名
+#sudo apt-get install --reinstall libsmbclient libsmbclient-dev libtevent0 libtalloc2
+#ps aux | grep smbd
+#[homes]
+#   comment = Home Directories
+#   browseable = yes
+#   public = yes
+#   read only = no
+#   create mask = 0775
+#   directory mask = 0775
+#   valid users = %S
+
+
 #oh my zsh
 sudo apt-get install -y zsh
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
